a986854beddbad9719b245efb8a8fad88e070637
Umair-Shxad <umairshehzad588@gmail.com>
2025-10-11 12:38:07 +0500

    update portfolio (wp update)

--- js/custom.js
+++ js/custom.js
@@ -1,79 +1,63 @@
-
-
 // Nav Toggle
 
 const navOpen = document.querySelector(".nav-toggle");
 
 navOpen.addEventListener("click", () => {
-    var nav = document.getElementById("nav");
-    nav.classList.toggle("nav-open");
+  var nav = document.getElementById("nav");
+  nav.classList.toggle("nav-open");
 });
 
-
-$(function(){
-
+$(function () {
   // Smooth Scrolling
-  
-  var smoothscroling = $(".scroll")
-  
-  console.log(smoothscroling);
-  smoothscroling.click(function(e){
-      e.preventDefault();
-      $("body,html").animate({
-          scrollTop: $(this.hash).offset().top + 10
-      })
+
+  var smoothscroling = $(".scroll");
+
+  smoothscroling.click(function (e) {
+    e.preventDefault();
+    $("body,html").animate({
+      scrollTop: $(this.hash).offset().top + 10,
+    });
   });
-  
+
   // Active link
-  
-  $(window).scroll(function(){
-      var scrollLocation = $(this).scrollTop();
-      smoothscroling.each(function(){
-          var sectionOffset = $(this.hash).offset().top
-  
-          if( sectionOffset <= scrollLocation ){
-              $(this).parent().addClass("active");
-              $(this).parent().siblings().removeClass("active");
-          }
-      })
+
+  $(window).scroll(function () {
+    var scrollLocation = $(this).scrollTop();
+    smoothscroling.each(function () {
+      var sectionOffset = $(this.hash).offset().top;
+
+      if (sectionOffset <= scrollLocation) {
+        $(this).parent().addClass("active");
+        $(this).parent().siblings().removeClass("active");
+      }
+    });
   });
-  
-  
-  
+
   // Fixed Nav
-  
+
   const nav = document.querySelector("#nav");
   const banner = document.querySelector("#banner");
-  
+
   const bannerOptions = {
-    rootMargin: "-550px 0px 0px 0px"
+    rootMargin: "-550px 0px 0px 0px",
   };
-  
-  const bannerObserver = new IntersectionObserver(function(
-    entries, 
+
+  const bannerObserver = new IntersectionObserver(function (
+    entries,
     bannerObserver
-    ) {
-  
-      entries.forEach(entry => {
-        if (!entry.isIntersecting){
-          nav.classList.add("scroll-nav");
-        } else {
-          nav.classList.remove("scroll-nav");
-        }
-      });
-  
-  }, 
+  ) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        nav.classList.add("scroll-nav");
+      } else {
+        nav.classList.remove("scroll-nav");
+      }
+    });
+  },
   bannerOptions);
-  
+
   bannerObserver.observe(banner);
-  
-  });
-
-
-
-
-
-
+});
 
 // Certificate Modal
 
@@ -89,7 +73,7 @@ var closeCModal = document.querySelector(".close-c-modal");
 // Open Certificate Modal
 cLink.addEventListener("click", openCertificateModal);
 
-function openCertificateModal(){
+function openCertificateModal() {
   cModal.classList.add("openModal");
   cModal.classList.remove("closeModal");
   document.body.classList.add("body_overflow");
@@ -98,7 +82,7 @@ function openCertificateModal(){
 // Close Certificate Modal
 closeCModal.addEventListener("click", closeCertificateModal);
 
-function closeCertificateModal(){
+function closeCertificateModal() {
   cModal.classList.add("closeModal");
   document.body.classList.remove("body_overflow");
 }
